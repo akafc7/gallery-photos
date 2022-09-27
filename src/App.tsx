8843657a49e40da7118468bf640fb17ce34066ee
Fernando Cavanellas <fernandocavanellas6@hotmail.com>
2022-09-27 15:57:48 -0300

    add photos in layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import * as C from './App.styles';
 import * as Photos from './services/photos';
 import { Photo } from './types/Photo';
+import { PhotoItem } from './components/PhotoItem';
 
 const App = () => {
     const [loading, setLoading] = useState(false);
@@ -27,6 +28,21 @@ const App = () => {
                     <C.ScreenWarning>
                         <div className="emoji">🤚</div>
                         <div>Carregando...</div>
+                    </C.ScreenWarning>
+                )}
+
+                {!loading && photos.length > 0 && (
+                    <C.PhotoList>
+                        {photos.map((item, index) => (
+                            <PhotoItem key={index} url={item.url} name={item.name} />
+                        ))}
+                    </C.PhotoList>
+                )}
+
+                {!loading && photos.length === 0 && (
+                    <C.ScreenWarning>
+                        <div className="emoji">🙁</div>
+                        <div>Não há fotos cadastradas</div>
                     </C.ScreenWarning>
                 )}
             </C.Area>
