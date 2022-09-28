6a03b58207b06eb402378c209cce4dc55d14712f
Fernando Cavanellas <fernandocavanellas6@hotmail.com>
2022-09-27 21:52:25 -0300

    uploading photo system

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, FormEvent } from 'react';
 import * as C from './App.styles';
 import * as Photos from './services/photos';
 import { Photo } from './types/Photo';
 import { PhotoItem } from './components/PhotoItem';
 
 const App = () => {
+  const [uploading, setUploading] = useState(false);
     const [loading, setLoading] = useState(false);
     const [photos, setPhotos] = useState<Photo[]>([]);
 
@@ -17,12 +18,36 @@ const App = () => {
         getPhotos();
     }, []);
 
+   const handleFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const file = formData.get('image') as File;
+    if (file && file.size > 0) {
+      setUploading(true);
+      let result = await Photos.insert(file);
+      setLoading(false);
+
+      if (result instanceof Error) {
+        alert(`${result.name} - ${result.message}`);
+      } else {
+        let newPhotoList = [...photos];
+        newPhotoList.push(result);
+        setPhotos(newPhotoList);
+      }
+    }
+   };
+  
+
     return (
         <C.Container>
             <C.Area>
                 <C.Header>Galeria de Fotos</C.Header>
 
-                {/*Area de upload*/}
+                <C.UploadForm  onSubmit={handleFormSubmit}>
+                    <input type="file" name="image" />
+                    <input type="submit" value="Enviar" />
+                </C.UploadForm>
 
                 {loading && (
                     <C.ScreenWarning>
@@ -34,7 +59,11 @@ const App = () => {
                 {!loading && photos.length > 0 && (
                     <C.PhotoList>
                         {photos.map((item, index) => (
-                            <PhotoItem key={index} url={item.url} name={item.name} />
+                            <PhotoItem
+                                key={index}
+                                url={item.url}
+                                name={item.name}
+                            />
                         ))}
                     </C.PhotoList>
                 )}
